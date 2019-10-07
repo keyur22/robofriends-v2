9cc36d9173bce5a64776b115923dc489d48e5251
Keyur Chaudhari <keyurchaudhari4@gmail.com>
2019-10-07 15:34:12 +0530

    Updates

---
+++ precache-manifest.6ac2cd892c9a67e1a0bb2b510176fe6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74cbe511fdfca10948dcf5b3e3298cb",
+    "url": "/robofriends-v2/index.html"
+  },
+  {
+    "revision": "bb5b0836882cc1690452",
+    "url": "/robofriends-v2/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7fcd9d133f660ff3d0be",
+    "url": "/robofriends-v2/static/css/main.b04883b0.chunk.css"
+  },
+  {
+    "revision": "bb5b0836882cc1690452",
+    "url": "/robofriends-v2/static/js/2.af74b29f.chunk.js"
+  },
+  {
+    "revision": "7fcd9d133f660ff3d0be",
+    "url": "/robofriends-v2/static/js/main.29261e33.chunk.js"
+  },
+  {
+    "revision": "58700aef814be7be53f9",
+    "url": "/robofriends-v2/static/js/runtime-main.1a8e13f9.js"
+  }
+]);
